7e2ce9f3bd3b4acd02bdc8cf7dac0041a6c0a9d5
JoshuaC33 <jctv33@live.co.uk>
2020-03-04 10:21:09 +0000

    Addition of: week 6 and Wif implimentation

--- week4_c/sloeber.ino.cpp
+++ week4_c/sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2020-02-18 12:13:28
+//This file has been generated on 2020-02-27 09:12:56
 
 #include "Arduino.h"
 #include "button.h"
--- week_6/week_6.ino
+++ week_6/week_6.ino
@@ -1,8 +1,10 @@
 #include "Arduino.h"
 #include "HttpClient.h"
 
-const char* PASS = "cjry3646";
-const char* SSID = "JcPhone";
+const char* PASS = "rteclrya";
+const char* SSID = "Jamie";
+
+WiFiServer server;
 
 void setup() {
 	Serial.begin(115200);
@@ -19,7 +21,10 @@ void setup() {
 
 	Serial.print("Connected as : ");
 	Serial.println(WiFi.localIP());
-	HTTP();
+
+	//HTTP();
+
+	server.begin(1337);
 
 }
 void HTTP() {
@@ -86,5 +91,67 @@ void HTTP() {
 }
 
 void loop() {
-
+	WiFiClient client = server.available();
+	if (client) {
+		delay(100);
+		handleClient(client);
+		client.stop();
+		Serial.println("--- Disconnected Client");
+	}
 }
+
+void processRequest(const String& header) {
+	if (header.indexOf("GET /helloworld") != -1) {
+		Serial.println("Hello World!");
+	}
+}
+
+void handleClient(WiFiClient& client) {
+	Serial.println(client.remoteIP());
+	String header;
+	header.reserve(512);
+	String currentLine;
+	currentLine.reserve(196);
+
+	while (client.connected()) {
+		char c = client.read();
+		if (c < 255) {
+			Serial.print(c);
+			header.concat(c);
+			if (c == '\n') {
+				// end of request - send response
+				if (currentLine.length() == 0) {
+					processRequest(header);
+					sendHeaders(client);
+					sendHTML(client);
+				} else {
+					currentLine = "";
+				}
+			} else if (c != '\r') {
+				currentLine += c;
+			}
+		} else { // JUNK - Break out!
+			break;
+		}
+	}
+}
+
+void sendHeaders(WiFiClient& client) {
+	client.println("HTTP/1.1 200 OK");
+	client.println("Content-type: text/html");
+	client.println("Connection: close");
+	client.println();
+	client.println("<!DOCTYPE html>");
+}
+
+void sendHTML(WiFiClient& client) {
+	client.println("<html>");
+	client.println("<head><title>ESP Web Server</title></html>");
+	client.println("<body>");
+	client.println("<h1>Welcome to the ESP server!</h1>");
+	client.print("<p> hasib is late </p> ");
+	client.println("</body>");
+	client.println("</html>");
+	client.println();
+}
+
